94d672cc7cbe903975946ac79dc3c0ff86caffb0
VictorGitHup <victorabj@gmail.com>
2023-11-02 22:53:47 -0500

    cambio url

--- main.py
+++ main.py
@@ -1,49 +1,73 @@
-from fastapi import FastAPI, UploadFile, File, HTTPException
-from fastapi.responses import StreamingResponse
-import io
-from vtracer import convert_image_to_svg_py
+from fastapi import FastAPI, UploadFile, File, HTTPException, BackgroundTasks
+from starlette.responses import FileResponse
+from pathlib import Path
 import shutil
-import os
+import uuid
 
-# Resto de tu código aquí
-
-
-# Resto de tu código aquí
-
+from vtracer import convert_image_to_svg_py
 
 app = FastAPI()
 
+# Directorio donde se guardarán los archivos convertidos de manera temporal
+temp_directory = "temp"
+output_directory = "converted"
+
+# Asegúrate de que los directorios existen
+Path(temp_directory).mkdir(exist_ok=True)
+Path(output_directory).mkdir(exist_ok=True)
+
 @app.post("/convert-to-svg/")
-async def convert_to_svg(file: UploadFile = File(...)):
+async def convert_to_svg(background_tasks: BackgroundTasks, file: UploadFile = File(...)):
     # Verificar el formato del archivo
     if file.content_type not in ["image/jpeg", "image/png"]:
         raise HTTPException(status_code=400, detail="Invalid file format.")
-
-    # Preparar el nombre del archivo de entrada y salida
-    file_extension = file.filename.split('.')[-1]
-    if file_extension.lower() not in ['jpg', 'jpeg', 'png']:
-        raise HTTPException(status_code=400, detail="Invalid file extension.")
     
-    input_path = f"temp/{file.filename}"
-    output_path = f"temp/{file.filename}.svg"
+    # Generar un nombre de archivo único para evitar colisiones
+    file_name = f"{uuid.uuid4()}.{file.filename.rsplit('.', 1)[-1].lower()}"
+    
+    input_path = Path(temp_directory) / file_name
+    output_path = Path(output_directory) / f"{file_name}.svg"
 
-    # Guarda el archivo subido en el sistema de archivos temporalmente
+    # Guardar el archivo subido temporalmente
     with open(input_path, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
 
+    # Llamar a la función de conversión en segundo plano
+    background_tasks.add_task(
+        convert_and_delete, input_path, output_path
+    )
+
+    # Generar un enlace a la imagen convertida
+    converted_url = f"/download/{file_name}.svg"
+    
+    # Devolver la URL completa como respuesta
+    return {"url": converted_url}
+
+@app.get("/download/{filename}")
+async def download_file(filename: str):
+    file_path = Path(output_directory) / filename
+    if not file_path.is_file():
+        raise HTTPException(status_code=404, detail="File not found.")
+    return FileResponse(path=file_path, filename=filename)
+
+async def convert_and_delete(input_path: Path, output_path: Path):
     # Convertir la imagen a SVG
-    convert_image_to_svg_py(input_path, output_path)
+    try:
+        convert_image_to_svg_py(input_path, output_path)
+    except Exception as e:
+        print(f"Error during conversion: {e}")
+        input_path.unlink(missing_ok=True)
+        raise HTTPException(status_code=500, detail="Error during conversion.")
+    finally:
+        # Eliminar el archivo de entrada temporal
+        input_path.unlink(missing_ok=True)
 
-    # Leer el archivo SVG resultante para enviarlo de vuelta al usuario
-    with open(output_path, "rb") as svg_file:
-        svg_content = svg_file.read()
+# Asegúrate de que el directorio de salida esté limpio al iniciar y al salir de la aplicación
+@app.on_event("startup")
+async def startup_event():
+    shutil.rmtree(output_directory, ignore_errors=True)
+    Path(output_directory).mkdir(exist_ok=True)
 
-    # Eliminar archivos temporales
-    os.remove(input_path)
-    os.remove(output_path)
-
-    # Crear una respuesta de streaming para enviar el SVG directamente
-    response = StreamingResponse(io.BytesIO(svg_content), media_type="image/svg+xml")
-    response.headers["Content-Disposition"] = f"attachment; filename={file.filename}.svg"
-
-    return response
+@app.on_event("shutdown")
+async def shutdown_event():
+    shutil.rmtree(output_directory, ignore_errors=True)

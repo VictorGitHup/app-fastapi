0655e324951c6e9fe4e8d25cfc36efd057c238ef
VictorGitHup <victorabj@gmail.com>
2023-11-09 00:25:13 -0500

    Json

--- main.py
+++ main.py
@@ -1,50 +1,64 @@
-from fastapi import FastAPI, UploadFile, File, HTTPException
-from fastapi.responses import FileResponse
-import vtracer
+from fastapi import FastAPI, File, UploadFile, HTTPException, Request
+from fastapi.responses import JSONResponse
 import os
-import uuid
-import aiofiles
-
+import shutil
+import vtracer  
+from starlette.responses import FileResponse
 
 app = FastAPI()
 
-# Directorio donde se guardarán los archivos convertidos de manera permanente
-output_directory = "converted"
-os.makedirs(output_directory, exist_ok=True)
+# Directorios para guardar los archivos de entrada y salida
+input_dir = "input_images"
+output_dir = "output_images"
 
-@app.post("/convert-to-svg/")
-async def convert_to_svg(file: UploadFile = File(...)):
-    # Verificar el formato del archivo
-    if file.content_type not in ["image/jpeg","image/jpg", "image/png"]:
-        raise HTTPException(status_code=400, detail="Invalid file format.")
+# Crear directorios si no existen
+os.makedirs(input_dir, exist_ok=True)
+os.makedirs(output_dir, exist_ok=True)
 
-    # Preparar el nombre del archivo de entrada y salida
-    filename_without_extension = os.path.splitext(file.filename)[0]
-    unique_filename = f"{filename_without_extension}_{uuid.uuid4()}"
-    input_path = os.path.join(output_directory, f"{unique_filename}.{file.content_type.split('/')[-1]}")
-    output_path = os.path.join(output_directory, f"{unique_filename}.svg")
+@app.post("/convert/")
+async def convert_image(request: Request, file: UploadFile = File(...)):
+    if file.content_type not in ["image/jpg", "image/png"]:
+        raise HTTPException(status_code=400, detail="Archivo no soportado. Solo se aceptan imágenes JPEG o PNG.")
 
-    # Guardar el archivo subido de manera temporal
-    async with aiofiles.open(input_path, 'wb') as out_file:
-        content = await file.read()  # async read
-        await out_file.write(content)  # async write
+    filename = os.path.basename(file.filename)
+    input_path = os.path.join(input_dir, filename)
+    output_filename = f"{os.path.splitext(filename)[0]}.svg"
+    output_path = os.path.join(output_dir, output_filename)
 
-    # Convertir la imagen a SVG
-    # Aquí estamos utilizando los valores predeterminados para la conversión
-    vtracer.convert_image_to_svg_py(input_path, output_path)
+    try:
+        # Guardar la imagen de entrada
+        with open(input_path, "wb") as image_file:
+            shutil.copyfileobj(file.file, image_file)
 
-    # Suponiendo que tienes un mecanismo para servir estos archivos, por ejemplo, un servidor estático
-    # La URL podría ser algo como 'http://<your-domain>/<path-to-static-files>/<output_filename>'
-    converted_url = f"https://18.218.25.223/converted/{unique_filename}.svg"
+        # Convertir la imagen a SVG
+        vtracer.convert_image_to_svg_py(input_path, output_path)
 
-    # Eliminar el archivo original después de la conversión para ahorrar espacio
-    os.remove(input_path)
+        # Generar URL de descarga para el archivo SVG
+        file_url = request.url_for('get_svg_image', path=output_filename)
 
-    # Devolver la URL completa como respuesta
-    return {"url": converted_url}
+        # Devolver la URL del archivo SVG convertido
+        return JSONResponse(
+            status_code=200,
+            content={
+                "url": str(file_url),
+                "filename": output_filename
+            }
+        )
 
-# Endpoint para servir los archivos convertidos
-@app.get("/converted/{filename}", response_class=FileResponse)
-async def get_converted_file(filename: str):
-    file_path = os.path.join(output_directory, filename)
-    return FileResponse(file_path)
+    except Exception as e:
+        # Manejo de errores
+        raise HTTPException(status_code=500, detail=f"Error durante la conversión: {str(e)}")
+
+    finally:
+        # Cerrar el archivo y eliminar los archivos de entrada
+        file.file.close()
+        os.remove(input_path)
+
+@app.get("/images/{path:path}", name="get_svg_image")
+async def get_svg_image(path: str):
+    output_path = os.path.join(output_dir, path)
+    if not os.path.isfile(output_path):
+        raise HTTPException(status_code=404, detail="Imagen no encontrada")
+    return FileResponse(output_path, media_type="image/svg+xml")
+
+# Aquí se pueden añadir más rutas y funcionalidades al API
